e0a3589ba78f02044f5d9678a2a8724ad05026a0
maretxaga <maretxaga@tolosaldelh.eus>
2023-09-30 17:29:05 +0200

    proba

---
+++ src/Nagusia.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+	public class Nagusia {
+	    public static void main(String[] args) {
+	    	long startTime = System.nanoTime();
+	        System.out.println("Kaixo mundua");	    
+	        long endTime = System.nanoTime() - startTime+5;
+	        System.out.println("Denbora: " + (endTime)/1e6 + " ms");
+	        }
+}
+	
+	
+	
